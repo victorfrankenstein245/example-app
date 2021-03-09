14c9961121fc4d4c3ac5cb7fb68a4a73da9ddad7
frankenstein245 <frankenstein245@gmail.com>
2021-03-09 14:36:23 -0500

    questions added

---
+++ lib/models/Questions.dart
@@ -0,0 +1,49 @@
+class Question {
+  final int id, answer;
+  final String question;
+  final List<String> options;
+
+  Question({this.id, this.question, this.answer, this.options});
+}
+
+const List sample_data = [
+  {
+    "id": 1,
+    "question":
+    "when did goole release flutter.",
+    "options": ["jun 2017", "jan 2017", "may 2017", "may 2018"],
+    "answer_index": 1,
+  },
+
+  {
+    "id": 2,
+    "question":
+    "when did goole release flutter.",
+    "options": ["jun 2017", "jan 2017", "may 2017", "may 2018"],
+    "answer_index": 2,
+  },
+
+  {
+    "id": 3,
+    "question":
+    "when did goole release flutter.",
+    "options": ["jun 2017", "jan 2017", "may 2017", "may 2018"],
+    "answer_index": 2,
+  },
+
+  {
+    "id": 4,
+    "question":
+    "when did goole release flutter.",
+    "options": ["jun 2017", "jan 2017", "may 2017", "may 2018"],
+    "answer_index": 2,
+  },
+
+  {
+    "id": 5,
+    "question":
+    "when did goole release flutter.",
+    "options": ["jun 2017", "jan 2017", "may 2017", "may 2018"],
+    "answer_index": 2,
+  }
+];

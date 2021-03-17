5e69bc79f133b2ad0f2ca5a7eec874438599a13d
victorfrankenstein245 <56684147+victorfrankenstein245@users.noreply.github.com>
2021-03-17 18:34:45 +0100

    quiz screen added

--- lib/screens/welcome/Welcome_screen.dart
+++ lib/screens/welcome/Welcome_screen.dart
@@ -9,67 +9,69 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        WebsafeSvg.asset(
-          "assets/icons/bg.svg",
-          width: MediaQuery.of(context).size.width,
-          // height: MediaQuery.of(context).size.height,
-          fit: BoxFit.cover,
-        ),
-        SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Spacer(flex: 2), // will take 2/6 space
-                Text(
-                  "lets play a game",
-                  style: Theme.of(context).textTheme.headline4.copyWith(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-                Text("enter ur information below"),
-                Spacer(), // will take 1/6 space
-
-                // input text field
-                TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xFF1C2341),
-                    hintText: "full names",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
-                    ),
+      body: Stack(
+        children: [
+          WebsafeSvg.asset(
+            "assets/icons/bg.svg",
+            width: MediaQuery.of(context).size.width,
+            // height: MediaQuery.of(context).size.height,
+            fit: BoxFit.cover,
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(flex: 2), // will take 2/6 space
+                  Text(
+                    "lets play a game",
+                    style: Theme.of(context).textTheme.headline4.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold),
                   ),
-                ),
-                Spacer(), // will take 1/6 space
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                    width: double.infinity,
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.all(kDefaultPadding * 0.85), //15
-                    decoration: BoxDecoration(
-                      gradient: kPrimaryGradient,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(12),
+                  Text("enter ur information below"),
+                  Spacer(), // will take 1/6 space
+
+                  // input text field
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFF1C2341),
+                      hintText: "full names",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
                       ),
                     ),
-                    child: Text(
-                      'Lets get started',
-                      style: Theme.of(context)
-                          .textTheme
-                          .button
-                          .copyWith(color: Colors.black),
+                  ),
+                  Spacer(), // will take 1/6 space
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(kDefaultPadding * 0.85), //15
+                      decoration: BoxDecoration(
+                        gradient: kPrimaryGradient,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(12),
+                        ),
+                      ),
+                      child: Text(
+                        'Lets get started',
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            .copyWith(color: Colors.black),
+                      ),
                     ),
                   ),
-                ),
-                Spacer(flex: 2), //it willtake 2/6 space
-              ],
+                  Spacer(flex: 2), //it willtake 2/6 space
+                ],
+              ),
             ),
-          ),
-        )
-      ]),
+          )
+        ],
+      ),
     );
   }
 }

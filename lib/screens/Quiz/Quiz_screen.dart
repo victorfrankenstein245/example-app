4401c43d3b97e262c7e6fcb35d5e99e7f36f0187
victorfrankenstein245 <56684147+victorfrankenstein245@users.noreply.github.com>
2021-03-21 23:20:08 +0100

    question page

--- lib/screens/Quiz/Quiz_screen.dart
+++ lib/screens/Quiz/Quiz_screen.dart
@@ -3,22 +3,11 @@ class  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Column(children: [
+        Text("second page")
+      ],),
     );
   }
 }
 
-class  extends StatefulWidget {
-  @override
-  _State createState() => _State();
-}
-
-class _State extends State<> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
-    );
-  }
-}
 
